0f45fcd8e54fdf19005ba6ed3eede93d2ef16c2d
Maxim Lesy <maxim.lesy@howest.be>
2026-03-24 14:54:10 +0100

    Initial repository

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    // GET DOM elements
+
+    // Add event listeners
+
+    // initialize the application
+    alert("test");
+}
